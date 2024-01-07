04f703d2bb535d6ca4b721374a5c30bf6a973d53
cplmills <91394219+cplmills@users.noreply.github.com>
2024-01-08 09:48:55 +1000

    week 1 complete

--- server.js
+++ server.js
@@ -9,11 +9,24 @@ const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const expressLayouts = require("express-ejs-layouts")
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+// Index Route
+app.get("/", function(req, res) {
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Local Server Information
