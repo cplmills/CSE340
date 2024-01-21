b3c748360d374c1bdfd22e432fe0bbcf780580bf
cplmills <91394219+cplmills@users.noreply.github.com>
2024-01-21 14:10:56 +1000

    Week 3 Working

--- server.js
+++ server.js
@@ -5,11 +5,13 @@
 /* ***********************
  * Require Statements
  *************************/
+const baseController = require("./controllers/baseController")
 const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const expressLayouts = require("express-ejs-layouts")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ***********************
  * View Engine and Templates
@@ -24,9 +26,11 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 // Index Route
-app.get("/", function(req, res) {
-  res.render("index", {title: "Home"})
-})
+// app.get("/", function(req, res) {
+//   res.render("index", {title: "Home"})
+// })
+app.get ("/", baseController.buildHome)
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
