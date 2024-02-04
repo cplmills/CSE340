1ec3606d821589e315ea4da4ef6ba7f8ae2a2021
cplmills <91394219+cplmills@users.noreply.github.com>
2024-02-04 16:22:35 +1000

    createde getaccountbyemail function

--- models/account-model.js
+++ models/account-model.js
@@ -28,14 +28,27 @@ async function checkExistingEmail(account_email){
 /* **********************
  *   process login
  * ********************* */
-async function processLogin(account_email, account_password){
+// async function processLogin(account_email, account_password){
+//   try {
+//     const sql = "SELECT * FROM account WHERE account_email = $1 AND account_password = $2"
+//     const login = await pool.query(sql, [account_email, account_password])
+//     return login.rowCount
+//   } catch (error) {
+//     return error.message
+//   }
+// }
+
+/* *****************************
+* Return account data using email address
+* ***************************** */
+async function getAccountByEmail (account_email) {
   try {
-    const sql = "SELECT * FROM account WHERE account_email = $1 AND account_password = $2"
-    const login = await pool.query(sql, [account_email, account_password])
-    return login.rowCount
+    const result = await pool.query(
+      'SELECT account_id, account_firstname, account_lastname, account_email, account_type, account_password FROM account WHERE account_email = $1',
+      [account_email])
+    return result.rows[0]
   } catch (error) {
-    return error.message
+    return new Error("No matching email found")
   }
 }
-
   module.exports = { registerAccount, checkExistingEmail}
