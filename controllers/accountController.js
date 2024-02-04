28805a42f252c5b8959f0ef9ecf2b4fc24feb49c
cplmills <91394219+cplmills@users.noreply.github.com>
2024-02-04 16:19:46 +1000

    create accountLogin function and secret key

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -17,33 +17,65 @@ async function buildLogin(req, res, next) {
 /* ****************************************
 *  Process Login
 * *************************************** */
-async function processLogin(req, res) {
-    let nav = await utilities.getNav()
-    const { account_email, account_password } = req.body
+// async function processLogin(req, res) {
+//     let nav = await utilities.getNav()
+//     const { account_email, account_password } = req.body
   
-    const regResult = await accountModel.processLogin(
-      account_email,
-      account_password
-    )
-    if (regResult) {
-      req.flash(
-        "notice",
-        `Congratulations, ${account_firstname}. Login Successful.`
-      )
-      res.status(201).render(".", {
-        title: "Logged In",
-        nav,
-        errors: null,
-      })
-    } else {
-      req.flash("notice", "Sorry, the login failed.")
-      res.status(501).render("account/login", {
-        title: "Login Here",
-        nav,
-        errors: null,
-      })
-    }
+//     const regResult = await accountModel.processLogin(
+//       account_email,
+//       account_password
+//     )
+//     if (regResult) {
+//       req.flash(
+//         "notice",
+//         `Congratulations, ${account_firstname}. Login Successful.`
+//       )
+//       res.status(201).render(".", {
+//         title: "Logged In",
+//         nav,
+//         errors: null,
+//       })
+//     } else {
+//       req.flash("notice", "Sorry, the login failed.")
+//       res.status(501).render("account/login", {
+//         title: "Login Here",
+//         nav,
+//         errors: null,
+//       })
+//     }
+//   }
+
+/* ****************************************
+ *  Process login request
+ * ************************************ */
+async function accountLogin(req, res) {
+  let nav = await utilities.getNav()
+  const { account_email, account_password } = req.body
+  const accountData = await accountModel.getAccountByEmail(account_email)
+  if (!accountData) {
+   req.flash("notice", "Please check your credentials and try again.")
+   res.status(400).render("account/login", {
+    title: "Login",
+    nav,
+    errors: null,
+    account_email,
+   })
+  return
   }
+  try {
+   if (await bcrypt.compare(account_password, accountData.account_password)) {
+   delete accountData.account_password
+   const accessToken = jwt.sign(accountData, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 3600 * 1000 })
+   res.cookie("jwt", accessToken, { httpOnly: true, maxAge: 3600 * 1000 })
+   return res.redirect("/account/")
+   }
+  } catch (error) {
+   return new Error('Access Forbidden')
+  }
+ }
+
+
+
 
   /* ****************************************
 *  Deliver registration view
